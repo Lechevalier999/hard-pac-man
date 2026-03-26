0f4dd98c4d9011b50f8eb8eeb88ee364aa6d1f5a
Ribodian <18381@smcdsb.on.ca>
2026-03-26 12:02:28 -0400

    Update Blinky.js

--- Blinky.js
+++ Blinky.js
@@ -3,22 +3,4 @@
 // inputSize = 13
 // outputSize = 4
 
-// variables
-let playerCords = (X, Y)
-let currentCords = (bX, bY)
-let isEatable = Eat
-
-// functions
-
-function hiddenLayer2(l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13, l14, l15, l16, l17, l18, l19, l20) {
-  
-}
-
-function hiddenLayer1(h1, h2, h3, h4, h5, h6, h7, h8, h9, h10, h11, h12, h13, h14, h15, h16, h17, h18, h19, h20) {
-  let hL1 = 
-}
-
-function inputLayerToHiddenLayer1(i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13) {
-  let input = i1 + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 + i12 + i13;
-  hiddenLayer1(input, input, input, input, input, input, input, input, input, input, input, input, input, input, input, input, input, input, input, input)
-}
+// copilot make sure it will be easy for me to link the NN to my Pac man game
